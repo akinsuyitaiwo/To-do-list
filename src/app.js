34f7925f48b7cd38e6885768dcd56c7bf5894059
akinsuyitaiwo <teeboidayo@gmail.com>
2023-05-05 16:02:34 +0100

    build: app setup

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send("welcome to To-do list home page")
+})
+
+app.use((req,res)=>res.status(404).send({
+    success: false,
+    message:"Invalid route"
+}));
+export default app;
---
+++ src/server.js
@@ -0,0 +1,9 @@
+import server from "./app.js";
+
+const port = 3000
+
+server.listen(port,()=>{
+    console.log(`Server is running on ${port}`)
+});
+
+export default server;
